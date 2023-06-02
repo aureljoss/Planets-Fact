22fb965d01327e6b6aa63e17d06e4dee6dfa7425
Aurelie <josserand.n.aurelie@gmail.com>
2023-06-01 23:07:13 -0400

    Fixes js link and image changes, button styles

--- scripts/app.js
+++ scripts/app.js
@@ -15,6 +15,9 @@ showOverview.addEventListener("click", () => {
   overview.style.display = "block";
   internal.style.display = "none";
   surface.style.display = "none";
+  showOverview.style.backgroundColor="rgb(65, 158, 187)";
+  showGeology.style.backgroundColor="";
+  showStructure.style.backgroundColor="";
 });
 
 showStructure.addEventListener("click", () => {
@@ -24,13 +27,18 @@ showStructure.addEventListener("click", () => {
   overview.style.display = "none";
   internal.style.display = "block";
   surface.style.display = "none";
+  showStructure.style.backgroundColor="rgb(65, 158, 187)";
+  showOverview.style.backgroundColor="";
+  showGeology.style.backgroundColor="";
 });
 
 showGeology.addEventListener("click", () => {
   structure.style.display = "none";
   description.style.display = "none";
   geology.style.display = "block";
-  overview.style.display = "none";
-  internal.style.display = "block";
-  surface.style.display = "none";
+  overview.style.display = "block";
+  internal.style.display = "none";
+  surface.style.display = "block";
+  showGeology.style.backgroundColor="rgb(65, 158, 187)";
+  showStructure.style.backgroundColor="";
 });
