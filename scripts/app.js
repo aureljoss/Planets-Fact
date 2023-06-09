9877aebcfbf1f4f1c31512d282d864bfcd0ab6fd
Aurelie <josserand.n.aurelie@gmail.com>
2023-06-08 22:43:41 -0400

    Adds js button color by planet

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,7 @@ const surface = document.getElementById("surface-geology-img");
 const showOverview = document.getElementById("showOverview");
 const showStructure = document.getElementById("showStructure");
 const showGeology = document.getElementById("showGeology");
+const planet = document.getElementById("planet");
 
 showOverview.addEventListener("click", () => {
   description.style.display = "block";
@@ -15,7 +16,24 @@ showOverview.addEventListener("click", () => {
   overview.style.display = "block";
   internal.style.display = "none";
   surface.style.display = "none";
-  showOverview.style.backgroundColor = "rgb(65, 158, 187)";
+  if (planet.textContent === "Mercury") {
+    showOverview.style.backgroundColor = "rgb(65, 158, 187)";
+  } else if (planet.textContent === "Venus") {
+    showOverview.style.backgroundColor = "rgb(237, 162, 73)";
+  } else if (planet.textContent === "Earth") {
+    showOverview.style.backgroundColor = "#6D2ED5";
+  } else if (planet.textContent === "Mars") {
+    showOverview.style.backgroundColor = "#D14C32";
+  } else if (planet.textContent === "Jupiter") {
+    showOverview.style.backgroundColor = "#D83A34";
+  } else if (planet.textContent === "Saturn") {
+    showOverview.style.backgroundColor = "#D14C32";
+  } else if (planet.textContent === "Uranus") {
+    showOverview.style.backgroundColor = "#1EC1A2";
+  } else {
+    showOverview.style.backgroundColor = "#2D68F0";
+  }
+
   showGeology.style.backgroundColor = "";
   showStructure.style.backgroundColor = "";
 });
